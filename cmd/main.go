0c1d4ebf6bc6baf0a4346749d4ec744434ed0929
karsh <utkarsh@cyberfear.com>
2024-03-19 10:48:31 +1100

    restructure switch
    config by design should have higher priority than other options (after
    help), to recognise user confusion while navigating through the utility
    trying to config things around, instead of really calling the actions

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+    // have support for both --help and -h for UNIX convention
     var callHelp bool
     flag.BoolVar(&callHelp, "help", false, "Show help message")
     flag.BoolVar(&callHelp, "h", false, "Short help message")
@@ -13,21 +14,26 @@ func main() {
     callBrowse := flag.Bool("browse", false, "browse images")
     callConfig := flag.Bool("config", false, "config setup")
     callDownload := flag.Bool("download", false, "download media")
+
+    // todo: add defaults for all other options
+    // todo: add file and directory support
+
     flag.Parse()
 
     switch {
     case callHelp:
 	menu.RunHelp()
 
-    case *callBrowse:
-	menu.RunBrowse()
-
     case *callConfig:
 	menu.RunConfig()
+
+    case *callBrowse:
+	menu.RunBrowse()
 
     case *callDownload:
 	menu.RunDownload()
 
+    // option to call by default when no other is explicitly stated
     default:
 	menu.RunUpload()
     }
