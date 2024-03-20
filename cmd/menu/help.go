7a241d0309a0b79b32443a4969f16135ed8aac59
karsh <utkarsh@cyberfear.com>
2024-03-20 17:34:59 +1100

    help usage
    basic help prompt that displays the most high-level sub-commands within
    the utility with their following argument types.
    
    later in the prompt these arugments would be listed with their
    descriptions

--- cmd/menu/help.go
+++ cmd/menu/help.go
@@ -2,10 +2,33 @@ package menu
 
 import (
     "fmt"
+    "github.com/fatih/color"
 )
 
 func RunHelp() {
-    fmt.Printf("help menu\n")
-}
+    heading := color.New(color.FgHiGreen).Add(color.Underline)
 
+    gray   := color.New(color.FgWhite).SprintFunc()
+    red    := color.New(color.FgRed).SprintFunc()
+    // yellow := color.New(color.FgHiYellow).SprintFunc()
+    // cyan   := color.New(color.FgHiCyan).SprintFunc()
+    // green  := color.New(color.FgHiGreen).SprintFunc()
+
+    // to ensure consistent layout for variable user-defined tab space
+    tabspace := "  "
+
+    // USAGE
+    heading.Print("USAGE:\n")
+    fmt.Printf("%s%s %s <FILE PATH>\n", tabspace,
+	red("streak"), gray("[UPLOAD OPTIONS]"))
+    
+    fmt.Printf("%s%s --browse     %s\n", tabspace,
+    	red("streak"), gray("[FILTERS]"))
+    
+    fmt.Printf("%s%s --download   %s\n",tabspace,
+    	red("streak"), gray("[FILTERS] <PATH>"))
+    
+    fmt.Printf("%s%s --config     %s\n", tabspace,
+    	red("streak"), gray("[PREFERENCES]"))
+}
 
