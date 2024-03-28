5ca19c7234259e1f5ff1c2665638b69287dbc483
karsh <utkarsh@cyberfear.com>
2024-03-29 03:32:14 +1100

    cobra init
    redefine all files as 'commands'. cobra defines a command as a
    'functionality' of the program.
    
    commands can have sub-commands, and can support flags- to modify its
    behaviour

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,13 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
-// helpCmd represents the help command
-var helpCmd = &cobra.Command{
-	Use:   "help",
+// rootCmd represents the root command
+var rootCmd = &cobra.Command{
+	Use:   "root",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,20 +16,21 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("help called")
+		fmt.Println("root called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(helpCmd)
+	rootCmd.AddCommand(rootCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// helpCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// rootCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// helpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
+
--- main.go
+++ main.go
@@ -1,41 +1,13 @@
 package main
 
 import (
-    "flag"
     "streak/cmd"
 )
 
 func main() {
-    // have support for both --help and -h for UNIX convention
-    var callHelp bool
-    flag.BoolVar(&callHelp, "help", false, "Show help message")
-    flag.BoolVar(&callHelp, "h", false, "Short help message")
+    // Commands represent actions, Args are things and Flags are modifiers for
+    // those actions.
 
-    callBrowse := flag.Bool("browse", false, "browse images")
-    callConfig := flag.Bool("config", false, "config setup")
-    callDownload := flag.Bool("download", false, "download media")
-
-    // todo: add defaults for all other options
-    // todo: add file and directory support
-
-    flag.Parse()
-
-    switch {
-    case callHelp:
-	cmd.RunHelp()
-
-    case *callConfig:
-	cmd.RunConfig()
-
-    case *callBrowse:
-	cmd.RunBrowse()
-
-    case *callDownload:
-	cmd.RunDownload()
-
-    // option to call by default when no other is explicitly stated
-    default:
-	cmd.RunUpload()
-    }
+    cmd.Execute()
 }
 
